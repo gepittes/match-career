6cb59098b6fe1ce3306a11c11b3070886d8d8344
Franklin William <ui.develloper@gmail.com>
2024-09-28 12:11:47 -0300

    Melhorias na seção Hero: otimização da estrutura do código e refinamento da legibilidade.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,33 +1,46 @@
-import Image from "next/image"
-import { Button } from "./ui/button"
+import Image from "next/image";
+import { Button } from "./ui/button";
+import ImagesMatch from "./ui/ImagesMatch";
 
 export const Hero = () => {
   return (
-    <section className="container mx-auto grid lg:grid-cols-2 py-20 md:py-32 gap-10 bg-slate-400">
-      <div className="text-center lg:text-start space-y-6">
-        <Image
-          src="/images/logo.svg"
-          alt="Logo"
-          width={300}
-          height={100}
-          priority
-        />
-        <h1>Dê match com a <span>carreira</span> dos seus sonhos!</h1>
-        <p>
-          Escolher a profissão certa nunca foi tão fácil e divertido. Faça nosso teste vocacional grátis e deixe a paixão pelos estudos te guiar na direção de um futuro promissor. Descubra as opções que a Unidesc oferece para você brilhar no mercado de trabalho.
-        </p>
-        <Button>Começar o teste agora!</Button>
-      </div>
-
-      <div className="flex justify-end">
-        <Image
-          src="/images/estudante-no-computador.png"
-          alt="Background Image"
-          width={410}
-          height={550}
-          priority
-        />
+    <section className="container mx-auto max-w-[1140px] mt-24">
+      <div className="flex flex-col">
+        <div className="flex md:justify-between gap-8 place-content-center p-5 lg:p-0 relative flex-wrap">
+          <div className="flex flex-col justify-center gap-4 md:w-1/2 ">
+            <Image
+              src="/images/logo.svg"
+              alt="Logo"
+              width={216}
+              height={100}
+              priority
+            />
+            <h1 className="text-3xl font-bold leading-normal">
+              Dê match com a{" "}
+              <span className="bg-primary bg-clip-text text-transparent">
+                carreira
+              </span>{" "}
+              dos seus sonhos!
+            </h1>
+            <p>
+              Escolher a profissão certa nunca foi tão fácil e divertido. Faça
+              nosso teste vocacional grátis e deixe a paixão pelos estudos te
+              guiar na direção de um futuro promissor. Descubra as opções que a
+              Unidesc oferece para você brilhar no mercado de trabalho.
+            </p>
+            <Button
+              variant={"primary"}
+              size={"default"}
+              className="text-base font-semibold md:w-3/5 py-0"
+            >
+              Começar o teste agora!
+            </Button>
+          </div>
+          <div className="relative">
+            <ImagesMatch />
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
---
+++ components/ui/ImagesMatch.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+
+const ImagesMatch = () => {
+  const img = [
+    {
+      src: "/images/estudante-no-computador.png",
+      alt: "Estudante no computador",
+      height: 400,
+      width: 300,
+      position: "relative",
+      top: "0",
+      left: "0",
+    },
+    {
+      src: "/images/gruaduation-icon.png",
+      alt: "Ícone de Graduação",
+      width: 238,
+      height: 238,
+      position: "absolute",
+      top: "0",
+      left: "-9rem",
+      floatAnimation: "animate-float1",
+    },
+    {
+      src: "/images/certificate.png",
+      alt: "Ícone de Certificado",
+      width: 132,
+      height: 135,
+      position: "absolute",
+      top: "-6rem",
+      left: "14rem",
+      floatAnimation: "animate-float2",
+    },
+    {
+      src: "/images/learning-online.png",
+      alt: "Ícone de Aprendizado Online",
+      width: 148,
+      height: 148,
+      position: "absolute",
+      top: "18rem",
+      left: "-7rem",
+      floatAnimation: "animate-float3",
+    },
+  ];
+
+  return (
+    <div className=" relative flex">
+      {img.map((image, index) => (
+        <Image
+          key={index}
+          style={{ top: image.top, left: image.left }}
+          src={image.src}
+          alt={image.alt}
+          width={image.width}
+          height={image.height}
+          className={`object-cover ${image.position} ${image.floatAnimation}`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ImagesMatch;
